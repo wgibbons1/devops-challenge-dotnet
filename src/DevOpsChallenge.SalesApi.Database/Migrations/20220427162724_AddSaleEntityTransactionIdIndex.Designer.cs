086e7ef9eea05ccd12293cae1d64df2af0c2d1be
wgibbons1 <tarquin16@hotmail.com>
2022-04-27 18:28:33 +0200

    Add migration for index on SaleEntity.TransactionId

--- src/DevOpsChallenge.SalesApi.Database/Migrations/20220427162724_AddSaleEntityTransactionIdIndex.Designer.cs
+++ src/DevOpsChallenge.SalesApi.Database/Migrations/20220427162724_AddSaleEntityTransactionIdIndex.Designer.cs
@@ -4,14 +4,16 @@ using DevOpsChallenge.SalesApi.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevOpsChallenge.SalesApi.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220427162724_AddSaleEntityTransactionIdIndex")]
+    partial class AddSaleEntityTransactionIdIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DevOpsChallenge.SalesApi.Database/Migrations/20220427162724_AddSaleEntityTransactionIdIndex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevOpsChallenge.SalesApi.Database.Migrations
+{
+    public partial class AddSaleEntityTransactionIdIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Sale_TransactionId",
+                table: "Sale",
+                column: "TransactionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sale_TransactionId",
+                table: "Sale");
+        }
+    }
+}
